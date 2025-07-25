7fd993adc2023ca5b210c445ef7b3a9bf62f4c41
byui-titus <titusrayz33@gmail.com>
2025-07-25 14:56:12 +0300

    done

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -113,7 +113,7 @@ export default class CheckoutProcess {
             // ✅ Clear the cart
             localStorage.removeItem(this.key);
             // ✅ Redirect to thank you page
-            window.location.href = "thank_you/index.html";
+            window.location.href = "/thank_you/index.html";
         } catch (err) {
             removeAllAlerts();
             for (let message in err.message) {
